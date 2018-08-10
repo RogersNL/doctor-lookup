36ebca432f8baf53ab525c9635cc7d7fe0edade4
“Nick <amellocello@msn.com>
2018-08-10 13:51:38 -0700

    API link established.

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -1,8 +1,10 @@
+import $ from 'jquery';
 export class Doctor{
   constructor(){}
   static getData(name, query, showResults, error){
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location=wa-seattle&user_location=37.773%2C-122.413&sort=best-match-desc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`)
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location=wa-seattle&user_location=37.773%2C-122.413&sort=best-match-desc&skip=0&limit=100&user_key=${process.env.exports.apiKey}`)
       .then(function(response){
+          console.log(response);
         showResults(response);
       }).fail(function(){
         error();
--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
-import { Doctor } from './Doctor';
+import { Doctor } from './doctor-lookup';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './animate.css';
 import './styles.css';
 
+//Functions
+function searchResults(response){
+
+  // $("#doctor-list").append(
+  //   `<h1>${response.}</h1>
+  //   <p>does this work?</p>`
+  //   );
+  }
+function error(){
+
+}
+//Form Submit
 $(document).ready(function(){
-  $("#search-form").submit
+  $("#search-form").submit(function(event){
+    event.preventDefault();
+    let inputtedName = $("#doctor-name").val();
+    let inputtedCondition = $("#condition").val();
+    Doctor.getData(inputtedName, inputtedCondition, searchResults, error);
+  });
 });
